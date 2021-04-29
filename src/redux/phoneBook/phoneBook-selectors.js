b96f4baa132b8ef2553fea27b2c28afbb7f14dde
Andrii-Velhan <velganaa@gmail.com>
2021-04-29 22:02:29 +0300

    add memoization to selector getVisibleContacts

--- src/redux/phoneBook/phoneBook-selectors.js
+++ src/redux/phoneBook/phoneBook-selectors.js
@@ -1,18 +1,35 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 const getAllItems = state => state.phoneBook.items;
 const getLoading = state => state.phoneBook.items;
 const getFilter = state => state.phoneBook.filter;
 
-const getVisibleContacts = state => {
-  const items = getAllItems(state);
-  const filter = getFilter(state);
-  const normalizedFilter = filter.toLowerCase();
+// without memoisation:
+// const getVisibleContacts = state => {
+//   const items = getAllItems(state);
+//   const filter = getFilter(state);
+//   const normalizedFilter = filter.toLowerCase();
 
-  let list = items.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter),
-  );
+//   let list = items.filter(({ name }) =>
+//     name.toLowerCase().includes(normalizedFilter),
+//   );
 
-  return list;
-};
+//   return list;
+// };
+
+// with memoisation:
+const getVisibleContacts = createSelector(
+  [getAllItems, getFilter],
+  (items, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+
+    let list = items.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter),
+    );
+
+    return list;
+  },
+);
 
 const phoneBookSelectors = {
   getAllItems,
